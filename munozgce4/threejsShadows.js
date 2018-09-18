7390a9c09fc4ca8e082d6bc24b08c335ac737d2d
Carlos Eduardo Muñoz González <carloseduardomunozgonzalez@MacBook-Pro-de-Carlos.local>
2018-09-18 01:16:42 -0500

    rotation Please enter the commit message for your changes. Lines starting

--- munozgce4/threejsShadows.js
+++ munozgce4/threejsShadows.js
@@ -1,141 +1,83 @@
-// 1. Enable shadow mapping in the renderer. 
-// 2. Enable shadows and set shadow parameters for the lights that cast shadows. 
-// Both the THREE.DirectionalLight type and the THREE.SpotLight type support shadows. 
-// 3. Indicate which geometry objects cast and receive shadows.
-
-var renderer = null, 
-scene = null, 
-camera = null,
-root = null,
-gun = null,
-monster = null,
-group = null,
-orbitControls = null;
-
-var objLoader = null, jsonLoader = null;
-
-var duration = 20000; // ms
+var renderer = null,
+    scene = null,
+    camera = null,
+    root = null,
+    monster = null,
+    group = null,
+    orbitControls = null;
+values = []
+var jsonLoader = null;
+var duration = 15; // ms
 var currentTime = Date.now();
-var animator = null;
-
+var animator=null;
+loopAnimation = true;
 
 function loadJson()
 {
     if(!jsonLoader)
-    jsonLoader = new THREE.JSONLoader();
-    
+        jsonLoader = new THREE.JSONLoader();
+
     jsonLoader.load(
         '../models/monster/monster.js',
 
         function(geometry, materials)
         {
             var material = materials[0];
-            
+
             var object = new THREE.Mesh(geometry, material);
             object.castShadow = true;
             object. receiveShadow = true;
-            object.scale.set(0.002, 0.002, 0.002);
-            object.position.y = -1;
-            object.position.x = 1.5;
+            object.scale.set(0.005, 0.005, 0.005);
+            object.position.y = -2;
+            object.position.x = 1;
             monster = object;
             scene.add(object);
             initAnimations();
-            playAnimations()
         },
         function ( xhr ) {
 
             console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    
+
         },
         // called when loading has errors
         function ( error ) {
-    
+
             console.log( 'An error happened' );
-    
+
         });
 }
 
-function loadObj()
-{
-    if(!objLoader)
-        objLoader = new THREE.OBJLoader();
-    
-    objLoader.load(
-        '../models/cerberus/Cerberus.obj',
 
-        function(object)
-        {
-            var texture = new THREE.TextureLoader().load('../models/cerberus/Cerberus_A.jpg');
-            var normalMap = new THREE.TextureLoader().load('../models/cerberus/Cerberus_N.jpg');
-            var specularMap = new THREE.TextureLoader().load('../models/cerberus/Cerberus_M.jpg');
-
-            object.traverse( function ( child ) 
-            {
-                if ( child instanceof THREE.Mesh ) 
-                {
-                    child.castShadow = true;
-                    child.receiveShadow = true;
-                    child.material.map = texture;
-                    child.material.normalMap = normalMap;
-                    child.material.specularMap = specularMap;
-                }
-            } );
-                    
-            gun = object;
-            gun.scale.set(3,3,3);
-            gun.position.z = -3;
-            gun.position.x = -1.5;
-            gun.rotation.x = Math.PI / 180 * 15;
-            gun.rotation.y = -3;
-            scene.add(object);
-        },
-        function ( xhr ) {
-
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    
-        },
-        // called when loading has errors
-        function ( error ) {
-    
-            console.log( 'An error happened' );
-    
-        });
-}
 
 function animate() {
 
     var now = Date.now();
-    var deltat = now - currentTime;
     currentTime = now;
-    var fract = deltat / duration;
-    var angle = Math.PI * 2 * fract;
 
-    if(gun)
-        gun.rotation.y += angle / 2;
 
-    if(monster)
-        monster.rotation.y += angle / 2;
+
 }
 
 function run() {
     requestAnimationFrame(function() { run(); });
-    
-        // Render the scene
-        renderer.render( scene, camera );
 
-        // Spin the cube for next frame
-        animate();
+    // Render the scene
+    renderer.render( scene, camera );
 
-        // Update the camera controller
-        orbitControls.update();
+    // Spin the cube for next frame
+    animate();
+    KF.update();
+
+    // Update the camera controller
+    orbitControls.update();
 }
 
 function setLightColor(light, r, g, b)
 {
     r /= 255;
-    g /= 255;
-    b /= 255;
-    
+    g /= 0;
+    b /= 125;
+
     light.color.setRGB(r, g, b);
 }
 
@@ -147,7 +89,7 @@ var mapUrl = "../images/checker_large.gif";
 var SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 2048;
 
 function createScene(canvas) {
-    
+
     // Create the Three.js renderer and attach it to our canvas
     renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
 
@@ -158,7 +100,7 @@ function createScene(canvas) {
     renderer.shadowMap.enabled = true;
     // Options are THREE.BasicShadowMap, THREE.PCFShadowMap, PCFSoftShadowMap
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    
+
     // Create a new Three.js scene
     scene = new THREE.Scene();
 
@@ -166,10 +108,10 @@ function createScene(canvas) {
     camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
     camera.position.set(-2, 6, 12);
     scene.add(camera);
-    
+
     // Create a group to hold all the objects
     root = new THREE.Object3D;
-    
+
     // Add a directional light to show off the object
     directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
 
@@ -187,15 +129,14 @@ function createScene(canvas) {
     spotLight.shadow.camera.near = 1;
     spotLight.shadow. camera.far = 200;
     spotLight.shadow.camera.fov = 45;
-    
+
     spotLight.shadow.mapSize.width = SHADOW_MAP_WIDTH;
     spotLight.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
 
     ambientLight = new THREE.AmbientLight ( 0x888888 );
     root.add(ambientLight);
-    
+
     // Create the objects
-    loadObj();
 
     loadJson();
 
@@ -216,41 +157,16 @@ function createScene(canvas) {
 
     mesh.rotation.x = -Math.PI / 2;
     mesh.position.y = -4.02;
-    
+
     // Add the mesh to our group
     group.add( mesh );
     mesh.castShadow = false;
     mesh.receiveShadow = true;
-    
-    // Create the cube geometry
-    geometry = new THREE.CubeGeometry(2, 2, 2);
-    
-    // And put the geometry and material together into a mesh
-    mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color:color}));
-    mesh.position.y = 3;
-    mesh.castShadow = true;
-    mesh.receiveShadow = false;
 
-    // Add the mesh to our group
-    group.add( mesh );
 
-    // Create the cylinder geometry
-    geometry = new THREE.CylinderGeometry(1, 2, 2, 50, 10);
-    
-    // And put the geometry and material together into a mesh
-    mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color:color}));
-    mesh.position.y = -3;
-
-    mesh.castShadow = true;
-    mesh.receiveShadow = false;
-
-    // Add the  mesh to our group
-    group.add( mesh );
-    
     // Now add the group to our scene
     scene.add( root );
 }
-
 function initAnimations()
 {
     animator = new KF.KeyFrameAnimator;
@@ -258,20 +174,34 @@ function initAnimations()
         interps:
             [
                 {
-                    keys:[0, .5, 1],
-                    values:[
-                            { y : 0 },
-                            { y : Math.PI  },
-                            { y :4 * Math.PI / 2 },
-                            ],
-                    target:monster.rotation
+                    keys:[0, 2],
+                    values: [
+                        { y: 0},
+                        { y: 2 *  Math.PI}
+                    ],
+                    target: monster.rotation
                 },
+                {
+                    keys:[1/5, 1.5/5, 2/5, 2.5/5,  3/5, 3.5/5, 4/5, 4.5/5,  5/5, 5.5/5,  6/5, 6.5/5,  7/5,
+                        7.5/5, 8/5, 8.5/5,  9/5, 9.5/5,  10/5, 10.5/5, 11/5],
+                    values:[
+                        { x : 0, z : 0}, { x : -0.75, z : 0.66}, { x : -1.5, z : 1.32}, { x : -2.25, z : 1.98}, { x : -2.75, z : 2.64 }, { x : -3.5, z : 3.3 }, { x : -4.25, z : 3.96 }, { x : -5, z : 4.62}, { x : -5.75, z : 5.2 }, { x : -6.5, z : 5.94 }, { x : -7.75, z : 6.6 }, { x : -8.5, z : 7.26 }, { x : -9.75, z : 7.92 }, { x : -11.25, z : 8.58}, { x : -12, z : 9.24 }, { x : -13.25, z : 9.9 }, { x : -14.75, z : 10.56 }, { x : -16, z : 11.22 }, { x : -17.25, z :11.88 }, { x : -18, z : 12.54 }, { x : -19.25, z : 13.2},
+
+                    ],
+
+                    target: monster.position
+                },
+
             ],
         loop: loopAnimation,
-        duration: duration * 1000,
-    });
-}
+        duration: duration * 1000
 
+    });
+
+
+    animator.start();
+
+}
 function playAnimations()
 {
     animator.start();
